07070edbf0294889e474888d2fece90866a7b937
jjw1707 <jjw1707@gmail.com>
2014-08-19 00:01:22 -0700

    pause.

--- app.py
+++ app.py
@@ -8,5 +8,21 @@ app = Flask(__name__)
 def hello_world(name=None):
   return render_template('hello.html', name=name)
 
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+  return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
